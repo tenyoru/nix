3bd6796124cebfd7a6898aa90238a0aae0935426
tenyoru <mail@tenyoru.io>
2026-03-01 01:54:54 +0100

    feat: update flake 2026-03-01 01:54:54

--- devices/phone/nix-on-droid.nix
+++ devices/phone/nix-on-droid.nix
@@ -18,10 +18,26 @@
   # Basic environment packages
   environment.packages = with pkgs; [
     git
+    openssh
     curl
     wget
   ];
 
   # Set default shell
   user.shell = "${pkgs.fish}/bin/fish";
+
+  # Enable SSH daemon
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
+  # Setup SSH directory
+  build.activation.sshd = ''
+    $DRY_RUN_CMD mkdir -p $HOME/.ssh
+    $DRY_RUN_CMD chmod 700 $HOME/.ssh
+  '';
 }
