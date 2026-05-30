fd853e9b7c5252b574e24d9947287e7361a80cfc
tenyoru <mail@tenyoru.io>
2026-05-30 14:01:09 +0200

    feat: update flake 2026-05-30 14:01:09

--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -6,11 +6,27 @@
 }: let
   niriDir = mylib.dotfileConfig "niri";
 in {
-  home.packages = [
-    pkgs.niri
-    pkgs.playerctl
-    pkgs.wl-clipboard
+  home.packages = with pkgs; [
+    niri
+    playerctl
+    wl-clipboard
+    xdg-utils
+    xdg-desktop-portal
   ];
+
+  xdg.portal = {
+    enable = true;
+    config = {
+      common = {
+        default = ["gnome"];
+        "org.freedesktop.impl.portal.FileChooser" = ["gtk"];
+      };
+    };
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+    ];
+  };
 
   xdg.configFile."niri".source =
     config.lib.file.mkOutOfStoreSymlink niriDir;
--- modules/host/wayland.nix
+++ modules/host/wayland.nix
@@ -1,23 +1,12 @@
 {pkgs, ...}: {
-  xdg.portal = {
-    enable = true;
-    config = {
-      common.default = ["gtk"];
-      niri = {
-        default = ["gnome" "gtk"];
-        "org.freedesktop.impl.portal.ScreenCast" = ["gnome"];
-        "org.freedesktop.impl.portal.Screenshot" = ["gnome"];
-      };
-    };
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-gnome
-    ];
-  };
+  programs.niri.enable = true;
 
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-  };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  environment.pathsToLink = [
+    "/share/applications"
+    "/share/xdg-desktop-portal"
+  ];
 
   environment.systemPackages = with pkgs; [
     xdg-utils
